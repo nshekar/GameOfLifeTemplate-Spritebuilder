513800ff2e3971e2ca62515d20c0ddee95e5b689
Neil Shekar <neilshekar@Neils-MacBook-Pro.local>
2014-08-27 22:26:21 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,14 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Neil Shekar on 8/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+@end
